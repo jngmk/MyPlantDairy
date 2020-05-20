f6c9cec8c27135a5ef636a13c9261c2f1441517e
jngmk <rlawjd7186@naver.com>
2020-05-20 20:13:31 +0900

    [feat] toggle the image gallery with an implicit intent.

--- app/src/main/java/edu/uc/jeong/myplantdiary/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/jeong/myplantdiary/ui/main/MainFragment.kt
@@ -1,18 +1,37 @@
 package edu.uc.jeong.myplantdiary.ui.main
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import edu.uc.jeong.myplantdiary.R
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
+
+    private lateinit var currentPhotoPath: String
+    private val IMAGE_GALLERY_REQUEST_CODE: Int = 2001
+    private val SAVE_IMAGE_REQUEST_CODE: Int = 1999
+    private val CAMERA_REQUEST_CODE: Int = 1998
+    private val CAMERA_PERMISSION_REQUEST_CODE = 1997
 
     companion object {
         fun newInstance() = MainFragment()
@@ -32,6 +51,101 @@ class MainFragment : Fragment() {
         viewModel.plants.observe(viewLifecycleOwner, Observer {
             plants -> actPlantName.setAdapter(ArrayAdapter(context!!, R.layout.support_simple_spinner_dropdown_item, plants))  // context, view, content
         })
+        btnTakePhoto.setOnClickListener {
+            prepTakePhoto()
+        }
+        btnLogon.setOnClickListener {
+            prepOpenImageGallery()
+        }
+    }
+
+    private fun prepOpenImageGallery() {
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+            type = "image/*"
+            startActivityForResult(this, IMAGE_GALLERY_REQUEST_CODE)
+        }
+    }
+
+    // See if we have permission or not
+    private fun prepTakePhoto() {
+        if (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePhoto()
+        } else {
+            val permissionRequest = arrayOf(android.Manifest.permission.CAMERA)  // arrayOf is to see array form
+            requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {  // like a case switch
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                    takePhoto()
+                } else {
+                    Toast.makeText(context, "Unable to take photo without permission", Toast.LENGTH_LONG).show()
+
+                }
+            }
+        }
+    }
+
+    private fun takePhoto() {
+        // also is like when in Kotlin
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+            takePictureIntent -> takePictureIntent.resolveActivity(context!!.packageManager)
+            if (takePictureIntent == null) {
+                Toast.makeText(context, "Unable to save photo", Toast.LENGTH_LONG).show()
+            } else {
+                // if we are here, we have a valid intent
+                val photoFile: File = createImageFile()
+                photoFile?.also {
+                    val photoURI = FileProvider.getUriForFile(activity!!.applicationContext, "com.myplantdiary.android.FileProvider", it)
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+                    startActivityForResult(takePictureIntent, SAVE_IMAGE_REQUEST_CODE)
+                }
+            }
+        }
+    }
+    // need annotation because createSource method and decodeBitmap method are required API 28 levels
+//    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                // now we can get the thumbnail
+                val imageBitmap = data!!.extras!!.get("data") as Bitmap
+                imgPlant.setImageBitmap(imageBitmap)
+            }
+            else if (requestCode == SAVE_IMAGE_REQUEST_CODE) {
+                Toast.makeText(context, "Image Saved", Toast.LENGTH_LONG).show()
+            }
+            else if (requestCode == IMAGE_GALLERY_REQUEST_CODE) {
+                if (data != null && data.data !== null) {  // data: stuff back to us, data.data: image uri user select
+                    val image = data.data
+                    val source = ImageDecoder.createSource(activity!!.contentResolver, image!!)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    imgPlant.setImageBitmap(bitmap)
+                }
+            }
+        }
+    }
+
+    private fun createImageFile(): File {
+        // generate unique file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+
+        // get access to the directory where we can write pictures.
+        val storageDir: File? = context!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        return File.createTempFile("PlantDiary${timeStamp}", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
     }
 
 }
